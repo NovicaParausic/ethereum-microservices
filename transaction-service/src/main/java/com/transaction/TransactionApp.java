e2f6b6ed795b51760393bdf943b5edf59958734a
Novica Parausic <novica.parausic@hotmail.com>
2020-09-18 10:13:18 +0200

    commit 1013918

---
+++ transaction-service/src/main/java/com/transaction/TransactionApp.java
@@ -0,0 +1,20 @@
+package com.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TransactionApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionApp.class, args);
+    }
+
+    @Bean
+    RestTemplate rest() {
+    	return new RestTemplate();
+    }
+
+}
