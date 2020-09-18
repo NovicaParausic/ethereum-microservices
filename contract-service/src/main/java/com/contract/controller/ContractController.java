63e87fdc691d61d81ec04ec930330ba8487efd31
Novica Parausic <novica.parausic@hotmail.com>
2020-09-18 10:45:21 +0200

    commit 1045918

---
+++ contract-service/src/main/java/com/contract/controller/ContractController.java
@@ -0,0 +1,29 @@
+package com.contract.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.contract.model.Contract;
+import com.contract.service.ContractService;
+
+@RestController
+@RequestMapping("/contract")
+public class ContractController {
+
+	@Autowired
+    ContractService service;
+    
+    @GetMapping("/owner")
+    public String getOwnerAccount() {
+    	return service.getOwnerAccount();
+    }
+    
+    @PostMapping
+    public Contract createContract(@RequestBody Contract newContract) throws Exception {
+    	return service.createContract(newContract);
+    }
+}
---
+++ contract-service/src/main/java/com/contract/model/Transaction.java
@@ -0,0 +1,23 @@
+package com.contract.model;
+
+public class Transaction {
+
+	private String contract;
+	private long amount;
+
+	public String getContract() {
+		return contract;
+	}
+
+	public void setContract(String contract) {
+		this.contract = contract;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+}
